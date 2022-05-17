5c7237eac39369954931c8ef3736738462c8d3f0
Desktop <Amansingh13611@gmail.com>
2022-05-17 20:45:50 +0530

    first commit

--- components/About.js
+++ components/About.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Image from 'next/image'
-import { AiOutlineAlibaba } from 'react-icons/Ai';
+
 
 
 const About = () => {
@@ -10,7 +10,7 @@ const About = () => {
         <div className="container  " >
             <div className="row d-flex align-items-center ">
                 <div className="aboutfont col-lg-7" data-aos="fade-right">
-                    <h3 className='green'> <AiOutlineAlibaba/> About us </h3>
+                    <h3 className='green'> About us </h3>
                     <h1 className='font_services'>We are top internet <br/> service company</h1>
                     <p className="py-2 para-light"> Aman ipsum dolor sit amet consectetur adipisicing elit. Non sed accusantium aut dolores inventore architecto modi cupiditate eligendi corporis, illum illo culpa. Reiciendis, molestias. Illum voluptatum quisquam ad veritatis dolorem.</p>
                     <p className="py-2 para-light">Lorem ipsum dolor sit amet consectetur adipisicing elit. Non sed accusantium aut dolores inventore architecto modi cupiditate eligendi corporis, illum illo culpa. Reiciendis, molestias. Illum voluptatum quisquam ad veritatis dolorem.</p>
